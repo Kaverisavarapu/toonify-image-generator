9bc858b17e28a7e9a5662bd65d0b0dde00c0ecf2
kaverisavarapu <kaverisavarapu@gmail.com>
2026-06-24 20:44:20 +0530

    Handle API errors safely

--- frontend/services/auth_api.py
+++ frontend/services/auth_api.py
@@ -5,26 +5,50 @@ BASE_URL = "https://toonify-image-generator-1.onrender.com"
 
 def login_user(email, password):
 
-    response = requests.post(
-        f"{BASE_URL}/login",
-        params={
-            "email": email,
-            "password": password
-        }
-    )
+    try:
 
-    return response.json()
+        response = requests.post(
+            f"{BASE_URL}/login",
+            params={
+                "email": email,
+                "password": password
+            }
+        )
+
+        print("LOGIN STATUS:", response.status_code)
+        print("LOGIN RESPONSE:", response.text)
+
+        return response.json()
+
+    except Exception as e:
+
+        return {
+            "success": False,
+            "message": str(e)
+        }
 
 
 def signup_user(username, email, password):
 
-    response = requests.post(
-        f"{BASE_URL}/signup",
-        params={
-            "username": username,
-            "email": email,
-            "password": password
-        }
-    )
+    try:
 
-    return response.json()
+        response = requests.post(
+            f"{BASE_URL}/signup",
+            params={
+                "username": username,
+                "email": email,
+                "password": password
+            }
+        )
+
+        print("SIGNUP STATUS:", response.status_code)
+        print("SIGNUP RESPONSE:", response.text)
+
+        return response.json()
+
+    except Exception as e:
+
+        return {
+            "success": False,
+            "message": str(e)
+        }
