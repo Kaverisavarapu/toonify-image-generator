cb1085d6a04931b405f2ecbf4b7aeabb17ed5f67
kaverisavarapu <kaverisavarapu@gmail.com>
2026-06-24 15:09:59 +0530

    deployment setup

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
 DATABASE_URL = (
-    "postgresql+psycopg2://postgres:Kaveri33%40@localhost:5432/toonify_db"
-)
+    "postgresql://neondb_owner:npg_moXf4HTy7BMh@ep-lucky-meadow-atijshg7-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
 engine = create_engine(DATABASE_URL)
 
