ec42a7adb55fff78a15c9981f41c3e6ee23e4d4d
kaverisavarapu <kaverisavarapu@gmail.com>
2026-06-24 17:26:16 +0530

    Fix torch hub trust issue

--- backend/anime.py
+++ backend/anime.py
@@ -6,13 +6,15 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 model = torch.hub.load(
     "bryandlee/animegan2-pytorch:main",
     "generator",
-    pretrained="celeba_distill"
-).to(device).eval()
+    pretrained="celeba_distill",
+    trust_repo=True
+)
 
 face2paint = torch.hub.load(
     "bryandlee/animegan2-pytorch:main",
     "face2paint",
-    size=512
+    size=512,
+    trust_repo=True
 )
 
 
